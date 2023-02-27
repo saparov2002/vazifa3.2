51b5fd9415a1b51b596f7e38bda2bed585673c7d
saparov2002 <123146210+saparov2002@users.noreply.github.com>
2023-02-27 22:04:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+ //vazifa__1
+
+
+
+// const birthYear = prompt(`When were your born?`)
+// if (birthYear>=2000){
+//     console.log('You were born in the 21st century')
+// }else{
+//     console.log(
+//         `You were born in the 20st century`
+//     )
+          
+// }
+
+
+//vazifa__2
+
+
+
+// let userFirstNumber = +prompt(`Biror raqam kiriting(x)`)
+// let userSecondNumer = +prompt(`yana bir raqam kiriting(y)`)
+// let total = +prompt(`bu raqamlar yigindisini hisoblang(x+y)`)
+
+// let totalRight = userFirstNumber + userSecondNumer;
+// if( total === totalRight ){
+//     console.log(`sizning javibinggiz to'g'ri`)
+// }
+// else{
+//     console.log(`afsuski siz adashtinggiz`)
+// }
+
+
+//vazifa__3
+
+// let totalAlisher = prompt(`Alisher iltimos qancha pulinggiz borligini kiriting`)
+
+// let oneDollar = 11000;
+// let oneYevro = 12000;
+
+// let airTicket = 500;
+// let hotel = 250;
+// let museumPlace = 120;
+
+// let ourCost = (hotel + airTicket)*oneDollar + museumPlace*oneYevro;
+
+// if(ourCost <= totalAlisher){
+//     console.log(`tabriklaymiz sizning yetarlicha pulinggiz bor`);
+// }else {
+//     console.log (`kechirasiz siz yana ozroq pul yigishinggiz kerak!`);
+// }
